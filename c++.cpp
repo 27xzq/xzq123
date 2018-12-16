7e0bcc35912e7d3e93b8af32383b7b3794bdb31f
27xzq <45909809+27xzq@users.noreply.github.com>
2018-12-16 13:58:07 +0800

    Create c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Sample
+{
+private:
+    int x;
+    static int y;
+public:
+    Sample(int a);
+    void print();
+};
+Sample::Sample(int a)
+{
+    x=a;
+    y=x++;
+}
+void Sample::print()
+{
+    cout<<"x="<<x<<",y="<<y<<endl;
+}
+int Sample::y=25;
+int main()
+{
+    Sample s1(5);
+    Sample s2(10);
+    s1.print();
+    s2.print();
+}
